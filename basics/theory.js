696c03208c5785da0ce10582aa615ec575ea9034
Kabileshwarankabil <kabileshwaran1896@gmail.com>
2026-05-02 21:01:55 +0530

    added basics code

---
+++ basics/theory.js
@@ -0,0 +1,27 @@
+let age = 23;
+let born = 2026-age;
+console.log(`Birth Year: ${born}`); // interpolation using template strings
+
+
+let dc=2.432;
+console.log(Math.round(dc));
+
+
+
+let num=Math.random()*10;
+console.log(Math.round(num));
+
+// typeof returns the data type of a data
+console.log(typeof 5);
+console.log(typeof 'Hello');
+console.log(typeof 45.43);
+console.log(typeof true);
+console.log(typeof 'B');
+
+// multi line string using template string
+let sentence=`
+Hello
+I am Kabileshwaran
+I am from Nuwara Eliya
+`;
+console.log(sentence);
